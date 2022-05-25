73d58df0268e075fd9716305cfc8f42e64a21315
Brenda <brendacassita@gmail.com>
2022-05-25 12:18:04 -0600

    init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-export const TEST_STRING = "test string";
+// DATA, DATA TYPES, INFORMATION
+// what do you think when I say PI number = 3.14 constant, circles
+// my age integer, number 0 - 120ish  
+// car horsepower, torque, model, yearmade: 1850-2022
+// students a list, student age:18-30 , grad, major, name:'asdfasdf asdfjlkj'
 
-export function testFunction() {
-  return TEST_STRING;
-}
+// 1 + 1  //2
+// 1 + 3  //4
+// 4 + 5  //9
+// 1 +    // ? you are using this wrong syntax error 
+
+// 1 + car // what ?
+// 1 + '1' // what ?
+// 1 + 1 456 // what ?
+
+// //+ takes two and exatcly 2 numbers return 1 number the sum
